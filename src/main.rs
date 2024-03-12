69ec9f0dfe1c14f5883ccec2c216835bf54bc9e5
Winston Cooke <git@winstoncooke.com>
2024-03-11 20:00:56 -0500

    complete chapter 3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use bracket_lib::prelude::*;
 use specs::{prelude::*, Component};
 use std::cmp::{max, min};
 
+const WIDTH: i32 = 80;
+const HEIGHT: i32 = 50;
+const AREA: i32 = WIDTH * HEIGHT;
+
+#[derive(Component, Debug)]
+struct Player {}
+
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -15,23 +22,120 @@ struct Renderable {
     bg: RGB,
 }
 
-#[derive(Component)]
-struct LeftMover {}
-
-#[derive(Component, Debug)]
-struct Player {}
-
 struct State {
     ecs: World,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        self.ecs.maintain();
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+
+        player_input(self, ctx);
+        self.run_systems();
+
+        let map = self.ecs.fetch::<Vec<TileType>>();
+        draw_map(&map, ctx);
+
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+#[derive(PartialEq, Copy, Clone)]
+enum TileType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * WIDTH as usize) + x as usize
+}
+
+fn new_map() -> Vec<TileType> {
+    let mut map = vec![TileType::Floor; AREA as usize];
+
+    // Make the boundary walls
+    for x in 0..WIDTH {
+        map[xy_idx(x, 0)] = TileType::Wall;
+        map[xy_idx(x, HEIGHT - 1)] = TileType::Wall;
+    }
+
+    for y in 0..HEIGHT {
+        map[xy_idx(0, y)] = TileType::Wall;
+        map[xy_idx(WIDTH - 1, y)] = TileType::Wall;
+    }
+
+    // Randomly place walls
+    let mut rng = RandomNumberGenerator::new();
+
+    for _i in 0..AREA {
+        let x = rng.roll_dice(1, WIDTH - 1);
+        let y = rng.roll_dice(1, HEIGHT - 1);
+        let idx = xy_idx(x, y);
+
+        if idx != xy_idx(WIDTH / 2, HEIGHT / 2) {
+            map[idx] = TileType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TileType], ctx: &mut BTerm) {
+    let mut x = 0;
+    let mut y = 0;
+
+    for tile in map.iter() {
+        match tile {
+            TileType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0.0, 0.0, 0.0),
+                    to_cp437('.'),
+                );
+            }
+            TileType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0.0, 0.0, 0.0),
+                    to_cp437('#'),
+                );
+            }
+        }
+
+        x += 1;
+        if x > WIDTH - 1 {
+            x = 0;
+            y += 1;
+        }
+    }
 }
 
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
-    
+    let map = ecs.fetch::<Vec<TileType>>();
+
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TileType::Wall {
+            pos.x = min(WIDTH - 1, max(0, pos.x + delta_x));
+            pos.y = min(HEIGHT - 1, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -52,45 +156,6 @@ fn player_input(gs: &mut State, ctx: &mut BTerm) {
     }
 }
 
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-
-        player_input(self, ctx);
-        self.run_systems();
-
-        let positions = self.ecs.read_storage::<Position>();
-        let renderables = self.ecs.read_storage::<Renderable>();
-
-        for (pos, render) in (&positions, &renderables).join() {
-            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
-        }
-    }
-}
-
-struct LeftWalker {}
-
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
-
-impl State {
-    fn run_systems(&mut self) {
-        let mut lw = LeftWalker {};
-        lw.run_now(&self.ecs);
-        self.ecs.maintain();
-    }
-}
-
 fn main() -> BError {
     let context: BTerm = BTermBuilder::simple80x50()
         .with_title("Roguelike")
@@ -99,11 +164,13 @@ fn main() -> BError {
     let mut gs: State = State { ecs: World::new() };
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position {
+            x: WIDTH / 2,
+            y: HEIGHT / 2,
+        })
         .with(Renderable {
             glyph: to_cp437('@'),
             fg: RGB::named(YELLOW),
@@ -111,19 +178,7 @@ fn main() -> BError {
         })
         .with(Player {})
         .build();
-
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: to_cp437('☺'),
-                fg: RGB::named(RED),
-                bg: RGB::named(BLACK),
-            })
-            .with(LeftMover {})
-            .build();
-    }
+    gs.ecs.insert(new_map());
 
     main_loop(context, gs)
 }
